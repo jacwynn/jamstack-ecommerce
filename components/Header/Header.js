8a50494d0b0dc0491f8b85fc553bcdcede2affac
jacwynn <jac@wynndigitalweb.com>
2021-08-11 14:57:34 -0400

    setup sass; add header/footer and container components/styles

---
+++ components/Header/Header.js
@@ -0,0 +1,20 @@
+import Container from '@components/Container';
+import styles from './Header.module.scss';
+
+import { FaShoppingCart } from 'react-icons/fa';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <Container className={styles.headerContainer}>
+                <p>Jamstack Store</p>
+                <p className={styles.headerCart}>
+                    <FaShoppingCart />
+                    $0.00
+                </p>
+            </Container>
+        </header>
+    )
+}
+
+export default Header;
