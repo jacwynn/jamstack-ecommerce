2bda5c57e5a93cc07e6e2a03bcc96a6fc72ecdea
jacwynn <jac@wynndigitalweb.com>
2021-08-12 22:58:40 -0400

    setup functionality to pull data from wordpress site via graphql

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql
+} from '@apollo/client';
 
 // components
 import Header from '@components/Header';
@@ -10,9 +15,9 @@ import Button from '@components/Button';
 import styles from '@styles/Home.module.scss';
 
 // data
-import products from '@data/products';
+// import products from '@data/products';
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -34,18 +39,19 @@ export default function Home() {
           <h2>Available Shoes</h2>
           <ul className={styles.productGrid}>
             {products.map(product => {
+              const { featuredImage } = product;
               return (
                 <li key={product.id}>
-                  <Image width="2048" height="1228" src={product.image} alt={product.title} />
+                  <Image width={featuredImage.mediaDetails.width} height={featuredImage.mediaDetails.height} src={featuredImage.sourceUrl} alt="Add in future" />
                   <h3>{product.title}</h3>
-                  <p>${product.price}</p>
+                  <p>${product.productPrice}</p>
                   <p><Button
                     className="snipcart-add-item"
-                    data-item-id={product.id}
-                    data-item-price={product.price}
+                    data-item-id={product.productId}
+                    data-item-price={product.productPrice}
                     data-item-url="/"
                     data-item-description=""
-                    data-item-image={product.image}
+                    data-item-image={featuredImage.sourceUrl}
                     data-item-name={product.title}
                   >
                     Add to Cart
@@ -65,4 +71,60 @@ export default function Home() {
       <div hidden id="snipcart" data-api-key={process.env.NEXT_PUBLIC_SNIPCART_API_KEY} />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: 'https://wynnsolutions.staging.jacwynn.com/wp/graphql',
+    cache: new InMemoryCache()
+  })
+
+  const res = await client.query({
+    query: gql`
+      query AllProducts {
+        products {
+          edges {
+            node {
+              id
+              content
+              title
+              uri
+              product {
+                productPrice
+                productId
+              }
+              slug
+              featuredImage {
+                node {
+                  sourceUrl
+                  mediaDetails {
+                    height
+                    width
+                  }
+                }
+              }
+            }
+          }
+        }
+      }    
+    `
+  });
+
+  const products = res.data.products.edges.map(({ node }) => {
+    const data = {
+      ...node,
+      ...node.product,
+      featuredImage: {
+        ...node.featuredImage.node
+      }
+    }
+    return data;
+  })
+
+
+  return {
+    props: {
+      products
+    }
+  }
 }
